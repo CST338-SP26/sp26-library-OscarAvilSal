ba1b0c95ffdba309b6f5fa618a61874aa69aeb2d
Oscar Aviles-Saldana <oaviles-saldana@csumb.edu>
2026-03-26 23:17:01 -0700

    created variables

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,8 @@
 public class Book {
+    public final static int ISBN_ = 0;
+    public final static int TITLE_ = 1;
+    public final static int SUBJECT_ = 2;
+    public final static int PAGE_COUNT_ = 3;
+    public final static int AUTHOR_ = 4;
+    public final static int DUE_DATE_ = 5;
 }
